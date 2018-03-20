0651230cee514b66e1050298f7644da468cee810
siguoya <924714558@qq.com>
2018-03-20 22:24:42 +0800

    轮播图组件

--- music/src/components/recommend/recommend.vue
+++ music/src/components/recommend/recommend.vue
@@ -1,20 +1,57 @@
 ﻿﻿<template>
     <div class="recommend">
-        推荐
+        <div class="scrollpic_list" ref="scrollpic_list">
+            <img class="scrollpic" v-for="slider_single in slider" :src="slider_single['picUrl']" alt="">
+        </div>
     </div>
 </template>
 <script type="text/ecmascript-6">
-    import getRecommend from 'api/recommend'
+	import getRecommend from 'api/recommend'
+	import BScroll from 'better-scroll'
+
 	export default {
 		name: "Recommend",
-        created(){
-            getRecommend().then((data)=>{
-                console.log(data)
-            },(err)=>{
-                console.log(err)
-            })
-        }
+		created() {
+			this.requestRecommendApi();
+		},
+		data() {
+			return {
+				slider: []
+			}
+		},
+		mounted() {
+		},
+		methods: {
+			requestRecommendApi() {
+				getRecommend().then((data) => {
+					this.getRecommendDataSuccess(data)
+				}, (err) => {
+					console.log(err)
+				})
+			},
+            getRecommendDataSuccess(data){
+	            this.slider = data['data']['slider']
+	            this.$nextTick(() => {
+	                this.setScrollpicDom()
+                    this.initBScroll()
+	            })
+            },
+			setScrollpicDom(){
+				this.$refs.scrollpic_list.style.width='2070px';
+			},
+			initBScroll(){
+				this.scroll = new BScroll('.recommend', {
+					scrollX: true,
+					scrollY: false
+				});
+            }
+		}
 	}
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
+    .scrollpic_list
+        display block
+        .scrollpic
+            display inline-block
+            width 414px
 </style>
