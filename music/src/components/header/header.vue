76adc999a279b5e61d2dd5da33e42ebe271da170
siguoya <924714558@qq.com>
2018-03-20 11:14:27 +0800

    代码锁紧，修复stylus定义出错的问题

--- music/src/components/header/header.vue
+++ music/src/components/header/header.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="header">
-      <p><img class="logo" alt="">Chicken Music</p>
-  </div>
+    <div class="header">
+        <p><img class="logo" alt="">Chicken Music</p>
+    </div>
 </template>
 <script>
 	export default {
 		name: "header"
 	}
 </script>
-<style lang="stylus" type="stylesheet/stylus">
-  @import '~common/stylus/variable'
-  @import "~common/stylus/mixin"
-  header
-    p
-      text-align center
-      color red
+<style lang="stylus" rel="stylesheet/stylus">
+    @import '~common/stylus/variable';
+    @import "~common/stylus/mixin";
+    header
+        p
+            text-align center
+            color red
 </style>
