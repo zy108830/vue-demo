bdfb6e3dc8ec2962503f57f8c4601eec66d83799
mypc <924714558@qq.com>
2018-03-25 23:12:26 +0800

    歌单列表原生滚动

--- music/src/App.vue
+++ music/src/App.vue
@@ -2,20 +2,42 @@
     <div id="app">
         <Header></Header>
         <Tab></Tab>
-        <keep-alive>
-            <router-view/>
-        </keep-alive>
+        <div class="scroll-wrapper" ref="scroll_wrapper">
+            <div class="content">
+                <keep-alive>
+                    <router-view/>
+                </keep-alive>
+            </div>
+        </div>
     </div>
 </template>
 <script type="text/ecmascript-6">
     import Header from 'components/header/header'
     import Tab from 'components/tab/tab'
+    import BScroll from 'better-scroll'
 	export default {
 		name: 'App',
 		data() {
 			return {
-
+                scroll:null
 			}
+		},
+		created(){
+
+		},
+		mounted(){
+            this.initScrollWrapperDom();
+//		    this.scroll=new BScroll('.scroll-wrapper',{
+//		        scrollX:false,
+//		        scrollY:true
+//		    })
+		},
+		methods:{
+            initScrollWrapperDom(){
+                let scrollWrapperHeight=window.innerHeight-88;
+                console.log(this.$refs,this.$refs.scroll_wrapper);
+                this.$refs.scroll_wrapper.style.height=scrollWrapperHeight+'px';
+            }
 		},
         components:{
             Header,
@@ -24,4 +46,6 @@
 	}
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
+    .scroll-wrapper
+        overflow: scroll
 </style>
