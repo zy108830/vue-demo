ee8ca9ff20e6942113496264b2ddda328b61d583
siguoya <924714558@qq.com>
2018-03-27 12:49:54 +0800

    反向代理的参数从后端移到前端，提高灵活性

--- music/build/webpack.dev.conf.js
+++ music/build/webpack.dev.conf.js
@@ -49,24 +49,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 					headers:{
 						referer: 'https://y.qq.com/portal/playlist.html'
 					},
-					params: {
-						"picmid": "1",
-						"rnd": "0.39075734482345137",
-						"g_tk": "1646990585",
-						"jsonpCallback": "getPlaylist",
-						"loginUin": "924714558",
-						"hostUin": "0",
-						"format": "json",
-						"inCharset": "utf8",
-						"outCharset": "utf-8",
-						"notice": "0",
-						"platform": "yqq",
-						"needNewCode": "0",
-						"categoryId": "10000000",
-						"sortId": "5",
-						"sin": "0",
-						"ein": "29"
-					},
+					params: req.query,
 					proxy:false
 				})
 				.then((response) => {
--- music/src/api/recommend.js
+++ music/src/api/recommend.js
@@ -14,14 +14,32 @@ export function getRecommend() {
 		uin: 0,
 		needNewCode: 1
 	}
-	return jsonp(url,data,{
+	return jsonp(url, data, {
 		param: 'jsonpCallback'
 	})
 }
 
 export function getDiscList() {
 	return axios({
-		url:'/getDiscList',
-		method:'get'
+		url: '/getDiscList',
+		method: 'get',
+		params: {
+			"picmid": "1",
+			"rnd": "0.39075734482345137",
+			"g_tk": "1646990585",
+			"jsonpCallback": "getPlaylist",
+			"loginUin": "924714558",
+			"hostUin": "0",
+			"format": "json",
+			"inCharset": "utf8",
+			"outCharset": "utf-8",
+			"notice": "0",
+			"platform": "yqq",
+			"needNewCode": "0",
+			"categoryId": "10000000",
+			"sortId": "5",
+			"sin": "0",
+			"ein": "29"
+		}
 	})
 }
