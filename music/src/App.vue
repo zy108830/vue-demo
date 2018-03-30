637ec7553f6850759f7d6e219c76229a9fba6f56
siguoya <924714558@qq.com>
2018-03-30 12:34:47 +0800

    热门歌单使用BScroll进行滚动

--- music/src/App.vue
+++ music/src/App.vue
@@ -1,47 +1,32 @@
 <template>
     <div id="app" @touchmove.prevent>
-        <div class="header">
+        <div class="app-header">
             <Header></Header>
             <Tab></Tab>
         </div>
-        <div class="scroll-wrapper" ref="scroll_wrapper">
-            <div class="content">
-                <keep-alive>
-                    <router-view/>
-                </keep-alive>
-            </div>
-        </div>
+        <keep-alive>
+            <router-view/>
+        </keep-alive>
     </div>
 </template>
 <script type="text/ecmascript-6">
 	import Header from 'components/header/header'
 	import Tab from 'components/tab/tab'
-	import BScroll from 'better-scroll'
-
 	export default {
 		name: 'App',
 		data() {
 			return {
-				scroll: null
+
 			}
 		},
 		created() {
+
 		},
 		mounted() {
-			this.initScrollWrapperDom();
-			setTimeout(() => {
-				this.scroll = new BScroll('.scroll-wrapper', {
-					scrollX: false,
-					scrollY: true
-				})
-			}, 20)
+
 		},
 		methods: {
-			initScrollWrapperDom() {
-				let scrollWrapperHeight = window.innerHeight - 88;
-				console.log(this.$refs, this.$refs.scroll_wrapper);
-				this.$refs.scroll_wrapper.style.height = scrollWrapperHeight + 'px';
-			}
+
 		},
 		components: {
 			Header,
@@ -51,12 +36,16 @@
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
     @import "~common/stylus/variable"
-    .header
-        position relative
-        z-index 10
+    #app
+        position absolute
+        top 0
+        left 0
+        width 100%
+        height 100%
         background-color $color-background
-
-    .scroll-wrapper
-        position relative
-        z-index 1
+    .app-header
+        position fixed
+        z-index 10
+        width 100%
+        background-color $color-background
 </style>
--- music/src/components/recommend/recommend.vue
+++ music/src/components/recommend/recommend.vue
@@ -1,22 +1,23 @@
-﻿﻿
-<template>
-    <div class="recommend">
-        <Scrollpic></Scrollpic>
-        <div class="disc-module">
-            <h1 class="disc-header">热门歌单推荐</h1>
-            <div class="disc-main">
-                <div class="disc-list">
-                    <div v-for="disc in disc_list" class="disc">
-                        <div class="disc-cover">
-                            <img :src="disc.imgurl" alt="">
-                        </div>
-                        <div class="disc-info">
-                            <p class="disc-author">
-                                {{disc.creator.name}}
-                            </p>
-                            <p class="disc-name">
-                                {{disc.dissname}}
-                            </p>
+﻿﻿﻿<template>
+    <div class="recommend" ref="recommend">
+        <div ref="recommend_content">
+            <Scrollpic></Scrollpic>
+            <div class="disc-module">
+                <h1 class="disc-header">热门歌单推荐</h1>
+                <div class="disc-main">
+                    <div class="disc-list">
+                        <div v-for="disc in disc_list" class="disc">
+                            <div class="disc-cover">
+                                <img :src="disc.imgurl" alt="">
+                            </div>
+                            <div class="disc-info">
+                                <p class="disc-author">
+                                    {{disc.creator.name}}
+                                </p>
+                                <p class="disc-name">
+                                    {{disc.dissname}}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -27,22 +28,46 @@
 <script type="text/ecmascript-6">
 	import Scrollpic from 'base/scrollpic/scrollpic'
 	import {getDiscList} from 'api/recommend'
+	import BScroll from 'better-scroll'
 
 	export default {
 		name: "Recommend",
+        props:{
+
+        },
 		data() {
 			return {
-				disc_list: []
-			}
+				disc_list:[]
+            }
 		},
 		created() {
 			getDiscList().then((resp) => {
 				const resp_content = resp['data'];
 				this.disc_list = resp_content['data']['list']
+                setTimeout(()=>{
+	                this.initScrollWrapperDom();
+                },5000)
 			}, (err) => {
 				console.log('error...');
 			})
 		},
+        mounted(){
+
+        },
+        methods:{
+	        initScrollWrapperDom() {
+	        	let recommend_height=window.innerHeight-88;
+	        	console.log('recommend_height',recommend_height);
+		        document.getElementsByClassName('recommend')[0].style.height=recommend_height+'px';
+		        this.scroll = new BScroll('.recommend', {
+			        scrollX: false,
+			        scrollY: true
+		        })
+	        }
+        },
+        watch:{
+
+        },
 		components: {
 			Scrollpic
 		}
@@ -51,6 +76,8 @@
 <style scoped lang="stylus" rel="stylesheet/stylus">
     @import "~common/stylus/variable"
     .recommend
+        position relative
+        top 88px
         .disc-module
             .disc-header
                 text-align center
